c5d59d1856f211b91f52d54b4d616e032a5ff4a3
Unknown <ryanbreitenfeldt@gmail.com>
2018-07-07 16:48:38 -0700

    made the first version of the congiurationsettings class.

---
+++ configurationsettings.cpp
@@ -0,0 +1,194 @@
+// configurationsetting class
+// rev0 7/06/18 by Ryan Breitenfeldt
+
+#include<string>
+#include<fstream>
+#include<iostream>
+#include<algorithm>
+#include<ctype.h>
+#include"configurationsettings.hpp"
+#include"uppercase.hpp"
+
+using namespace std;
+
+int ConfigurationSettings::getMaximumNumberOfTransitions(){
+    return maximumNumberOfTransitions;
+};
+int ConfigurationSettings::getMaximumNumberOfCells(){
+    return maximumNumberOfCells;
+};
+string ConfigurationSettings::getDisplayFullPath(){
+    return displayFullPath;
+};
+void ConfigurationSettings::setMaximumNumberOfTransitions(int maximumNumberOfTransitionsInput){
+    maximumNumberOfTransitions = maximumNumberOfTransitionsInput;
+};
+void ConfigurationSettings::setMaximumNumberOfCells(int maximumNumberOfCellsInput){
+    maximumNumberOfCells = maximumNumberOfCellsInput;
+};
+void ConfigurationSettings::toggleDisplayFullPath(){
+    if(displayFullPath.compare("yes"))
+    {
+        displayFullPath = "no";
+    }else{
+        displayFullPath = "yes";
+    }
+};
+
+//this method loads the config file for the pushdown automaton
+//and sets defaults if any are wrong or missing 
+void ConfigurationSettings::load(){
+
+    //string filename = "pda.cfg";
+    ifstream config("pda.cfg");
+    string configline;
+    size_t found;
+    bool tranNotFound = true;
+    bool displayNotFound = true;
+    bool charNotFound = true;
+    bool firstNumFound = false;
+    bool spaceAfterNum = false;
+    bool invalidString = false; 
+
+    //set the defaults
+    maximumNumberOfCells=32;
+    maximumNumberOfTransitions=1;
+    displayFullPath="no";
+
+    //config.open(filename.c_str());
+
+    if(config.is_open()){
+        while(!config.eof())
+        {
+            getline(config,configline);
+
+            //convert to uppercase
+            configline = uppercase(configline);
+
+            // remove whitespace here
+            //configline.erase(remove(configline.begin(), configline.end(), ' '), configline.end());
+
+
+            found = configline.find("=");
+            //cheak for a  = sign on the line
+            if(found != string::npos){
+
+                //cheaks for the number of transitins setting
+
+                found = configline.find("MAXIMUM_TRANSITIONS");
+                if(found != string::npos){
+                    //configline.erase(remove(configline.begin(), configline.end(), ' '), configline.end());
+                    found = configline.find("=");
+                    if(found+1 != configline.length()){
+
+                        configline = configline.substr(found+1,configline.length()-1);
+                        if(configline.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") != string::npos){
+                            continue;
+                        }
+
+                        for(int i=0;i<(configline.length())-1;i++){
+                            if(isdigit(configline[i])  && !firstNumFound){
+                                firstNumFound = true;
+                            
+                            }
+                            if(isdigit(configline[i]) && firstNumFound && spaceAfterNum){
+                                invalidString = true;
+                                break;
+                            }
+                            if(isspace(configline[i]) && firstNumFound){
+                                spaceAfterNum = true;
+                            }
+
+                        }
+                        if(invalidString){
+                            invalidString = false;
+                            continue;
+                        }
+                        
+                        if(tranNotFound && (stoi(configline,nullptr) > 0)){
+                            maximumNumberOfTransitions= stoi(configline,nullptr);
+                            tranNotFound = false;
+                        }
+                    }
+
+
+                }
+
+                //cheaks for the charaters to truncate setting
+
+                found = configline.find("MAXIMUM_CHARACTERS");
+                if(found != string::npos){
+                    //configline.erase(remove(configline.begin(), configline.end(), ' '), configline.end());
+                    found = configline.find("=");
+                    if(found+1 != configline.length()){
+                        configline = configline.substr(found+1,configline.length()-1);
+
+                        if(configline.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ") != string::npos){
+                            continue;
+                        }
+
+                        for(int i=0;i<(configline.length())-1;i++){
+                            if(isdigit(configline[i])  && !firstNumFound){
+                                firstNumFound = true;
+                            
+                            }
+                            if(isdigit(configline[i]) && firstNumFound && spaceAfterNum){
+                                invalidString = true;
+                                break;
+                            }
+                            if(isspace(configline[i]) && firstNumFound){
+                                spaceAfterNum = true;
+                            }
+
+                        }
+                        if(invalidString){
+                            invalidString = false;
+                            continue;
+                        }
+
+
+
+                        if(charNotFound && (stoi(configline,nullptr) > 0)){
+                            maximumNumberOfCells= stoi(configline, nullptr);
+                            charNotFound = false;
+                        }
+
+                    }
+                    
+
+                }
+
+                //cheaks for the display paths setting 
+                found = configline.find("COMPLETE_PATHS");
+                if(found != string::npos){
+                    found = configline.find("YES");
+                    if(found != string::npos && displayNotFound){
+                        displayFullPath="yes";
+                        displayNotFound = false;
+                        
+                    }
+                    found = configline.find("NO");
+                    if(found != string::npos && displayNotFound){
+                        displayNotFound = false;
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+
+};
+void ConfigurationSettings::writeFile(){
+    ofstream configFile("pda.cfg");
+
+    if(configFile.is_open()){
+        configFile << "MAXIMUM_TRANSITIONS=" << maximumNumberOfTransitions << endl;
+        configFile << "MAXIMUM_CHARACTERS=" << maximumNumberOfCells << endl;
+        configFile << "COMPLETE_PATHS=" << displayFullPath << endl;
+    }
+
+
+};
---
+++ configurationsettings.hpp
@@ -0,0 +1,33 @@
+// configurationsetting class
+// rev0 7/06/18 by Ryan Breitenfeldt
+
+#ifndef CONFIGURATIONSETTINGS_HPP
+#define CONFIGURATIONSETTINGS_HPP
+
+#include<string>
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+
+//class ConfigurationSettings
+class ConfigurationSettings
+{
+    private:
+        int maximumNumberOfTransitions;
+        int maximumNumberOfCells;
+        string displayFullPath;
+
+    public:
+        int getMaximumNumberOfTransitions();
+        int getMaximumNumberOfCells();
+        string getDisplayFullPath();
+        void setMaximumNumberOfTransitions(int maximumNumberOfTransitionsInput);
+        void setMaximumNumberOfCells(int maximumNumberOfCellsInput);
+        void toggleDisplayFullPath();
+        void load();
+        void writeFile();
+};
+
+
+#endif
