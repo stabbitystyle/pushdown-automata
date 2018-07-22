899e9aa4934d38810d57d76b4ef964e46807bf9d
Woodford <cole.woodford@elec.tricity.wsu.edu>
2018-07-22 15:56:58 -0700

    Fixed a setting for operating=false once an input string is accepted or rejected.

--- pushdownautomata.cpp
+++ pushdownautomata.cpp
@@ -377,10 +377,12 @@ string PushdownAutomata::initialize(string inputString)
 		if (accepted)
 		{
 			cout << "Input string " << originalInputString << " accepted in " << numberOfTransitions << " transitions with " << numberOfCrashes << " crashes." << endl;
+			operating = false;
 		}
 		if (rejected)
 		{
 			cout << "Input string " << originalInputString << " rejected in " << numberOfTransitions << " transitions with " << numberOfCrashes << " crashes." << endl;
+			operating = false;
 		}
 	}else{
 		cout << "Input string " << originalInputString << " neither accepted or rejected in " << numberOfTransitions << " transitions with " << numberOfCrashes << " crashes." << endl;
